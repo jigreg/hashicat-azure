9f9a4282a784d3da8c59e91fd9148f2c825c7176
jigreg <jigreg971211@gmail.com>
2023-11-23 04:32:39 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.41.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "seojun"
   location = "Southeast Asia"
